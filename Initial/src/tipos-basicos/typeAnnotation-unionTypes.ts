da0840bf1ed34c312588e3ea827fdb2523b0d47f
Manaure Vasconcelos <manaurevasc97@gmail.com>
2024-03-20 21:02:43 -0300

    new types basic

--- Initial/src/tipos-basicos/typeAnnotation-unionTypes.ts
+++ Initial/src/tipos-basicos/typeAnnotation-unionTypes.ts
@@ -27,3 +27,16 @@ function sum(x: number, y: number): number {
 }
 
 const sum2: (x: number, y: number) => number = (x, y) => x + y;
+
+// Union types                                                    ou
+// usa o operador | para dizer que a variável ou retorno é number | string
+function addOrConcat(x: number | string, y: number | string){
+  if(typeof x === 'number' && typeof y === 'number') return x + y;
+  if(typeof x === 'string' && typeof y === 'string') return x + y; // Se adc essa segunda linha e retirar a seguinte, o retorno da função será 3 opções => string, number, undefined
+  return ` ${x} + ${y} string`; // ao adc essa linha, abrange as outras possibilidades e elimina o undefined.
+}
+
+console.log(addOrConcat(10 ,20))
+console.log(addOrConcat('10' ,'20'))
+console.log(addOrConcat(10 ,'20'))
+console.log(addOrConcat('10' ,20))
